09e9a4866d9c21249c487df3d655c14626eb41be
WazirKazimi <mirwazirkazimi@gmail.com>
2024-09-22 22:54:54 +0530

    added cart page functionality

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,5 +1,12 @@
 // cart.js
-export const cart = [];
+export const cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    Quantity: 2
+},
+{
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    Quantity: 2
+}];
 //To check for the repeated item in the cart and increament accordingly
 export function Idcheck(productId) {
   let matchingItem;
